88d4f3addf0939870893b105e1c9dd9c5df27c8c
Dmitry Muntyan <muntyan.co@gmail.com>
2018-04-01 19:28:54 +0300

    Before push

--- src/script.js
+++ src/script.js
@@ -2,6 +2,13 @@ var demo = document.getElementById('demo');
 
 var a = [1,2,3,4];
 
+a.prop = '123';
+console.log(a);
+
+a.push(5);
+a.push(6);
+console.log(a);
+
 var obj = {
 	name: 'Name',
 	surname: 'Surname',
@@ -15,4 +22,11 @@ Object.defineProperty(obj, 'prop', {
 	value: 'testProp'
 });
 
+demo.addEventListener('click', function() {
+	alert('Clicked...');
+});
+
 console.log('>>>>>>', 'Hello world!!!', '<<<<<<');
+
+demo.append("Hello, world!");
+demo.after("New Hello, world!");
